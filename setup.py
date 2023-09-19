64a0ed6f0bf1cb430d1294091d1bafffbc2ce17b
Ralph Debusmann <ralph.debusmann@mgb.ch>
2023-09-19 17:25:01 +0200

    updated setup.py to Python 3.10

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.10',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
 )
