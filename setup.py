af890952937197fba36beb1949aaba2d54667f67
Ralph M. Debusmann <matthias.debusmann@gmail.com>
2023-11-14 18:50:58 +0100

    0.0.13

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='kafi',
-    version='0.0.12',
+    version='0.0.13',
     description='Kafka and Files',
     long_description=long_description,
     long_description_content_type='text/markdown',
