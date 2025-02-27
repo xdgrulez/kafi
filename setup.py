a83f3f716d5ae73be1b61c3f09a7c90ac78304b3
Ralph Debusmann <ralph.debusmann@mgb.ch>
2025-02-27 11:36:50 +0100

    Debug output for ts, end_ts

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='kafi',
-    version='0.0.44',
+    version='0.0.45',
     description='Kafka and Files',
     long_description=long_description,
     long_description_content_type='text/markdown',
