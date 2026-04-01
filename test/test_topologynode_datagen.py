959f5c5ac29ab7d10f607f20f7d13c4ced6998ed
Ralph Debusmann <ralph.debusmann@mgb.ch>
2026-04-01 14:26:00 +0200

    3 join almost works...not quite

--- test/test_topologynode_datagen.py
+++ test/test_topologynode_datagen.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 import os
 import sys
@@ -31,7 +32,6 @@ class TestTopologyNodeDatagen(unittest.TestCase):
 
     #
 
-    # Would have produced 15058 outputs in 42.62823438644409
     def test_1_join(self):
         click_topic_str = "shoe_clickstream"
         customer_topic_str = "shoe_customers"
@@ -101,7 +101,6 @@ class TestTopologyNodeDatagen(unittest.TestCase):
         print(f"Would have produced {n} outputs in {end_time_int - start_time_int}")
 
 
-    # Would have produced 20764 outputs in 174.58808875083923
     def test_2_joins(self):
         click_topic_str = "shoe_clickstream"
         customer_topic_str = "shoe_customers"
@@ -190,5 +189,131 @@ class TestTopologyNodeDatagen(unittest.TestCase):
         click_consumer.close()
         customer_consumer.close()
         product_consumer.close()
+        #
+        print(f"Would have produced {n} outputs in {end_time_int - start_time_int}")
+
+    def test_3_joins(self):
+        click_topic_str = "shoe_clickstream"
+        customer_topic_str = "shoe_customers"
+        product_topic_str = "shoes"
+        order_topic_str = "shoe_orders"
+        #
+        source_click_topologyNode = source(click_topic_str)
+        source_customer_topologyNode = source(customer_topic_str)
+        source_product_topologyNode = source(product_topic_str)
+        source_order_topologyNode = source(order_topic_str)
+        #
+
+        def within_n_minutes(ts1: str, ts2: str, n: int) -> bool:
+            dt1 = datetime.datetime.fromisoformat(ts1)
+            dt2 = datetime.datetime.fromisoformat(ts2)
+            diff = abs((dt1 - dt2).total_seconds())
+            return diff <= n * 60
+
+        clickstream_topologyNode = (
+            source_click_topologyNode
+            .map(lambda x: {"user_id": x["user_id"], "ip": x["ip"], "product_id": x["product_id"], "click_ts": x["ts"]})
+        )
+        #
+        customers_topologyNode = (
+            source_customer_topologyNode
+            .map(lambda x: {"id": x["id"], "first_name": x["first_name"]})
+        )
+        #
+        product_topologyNode = (
+            source_product_topologyNode
+            .map(lambda x: {"id": x["id"], "brand": x["brand"]})
+        )
+        #
+        order_topologyNode = (
+            source_order_topologyNode
+            .map(lambda x: {"order_id": x["order_id"], "product_id": x["product_id"], "customer_id": x["customer_id"], "order_ts": x["ts"]})
+        )
+        #
+        root_topologyNode = (
+            clickstream_topologyNode
+            .join(customers_topologyNode,
+                  on_function=lambda l, r: l["user_id"] == r["id"],
+                  projection_function=lambda l, r: {"user_id": l["user_id"],
+                                                    "ip": l["ip"],
+                                                    "click_ts": l["click_ts"],
+                                                    "product_id": l["product_id"],
+                                                    "first_name": r["first_name"]})
+                  .join(product_topologyNode,
+                        on_function=lambda l, r: l["product_id"] == r["id"],
+                        projection_function=lambda l, r: {"user_id": l["user_id"],
+                                                          "ip": l["ip"],
+                                                          "click_ts": l["click_ts"],
+                                                          "product_id": l["product_id"],
+                                                          "first_name": l["first_name"],
+                                                          "brand": r["brand"]})
+                        .join(order_topologyNode,
+                              on_function=lambda l, r: l["product_id"] == r["product_id"] and
+                              l["user_id"] == r["customer_id"] and
+                              within_n_minutes(l["click_ts"], r["order_ts"], 10),
+                              projection_function=lambda l, r: {"user_id": l["user_id"],
+                                                                "ip": l["ip"],
+                                                                "click_ts": l["click_ts"],
+                                                                "product_id": l["product_id"],
+                                                                "first_name": l["first_name"],
+                                                                "brand": l["brand"],
+                                                                "order_id": r["order_id"],
+                                                                "order_ts": r["order_ts"]},
+                              profile_config_dict = None)
+                        # profile_config_dict = {"gc": {"memory": {"after": True, "delta": True}, "streams": "size"}, "include": []})
+        )
+        #
+        cluster = Cluster("local")
+        cluster.consume_batch_size(1000)
+        click_consumer = cluster.consumer(click_topic_str, value_type="avro")
+        customer_consumer = cluster.consumer(customer_topic_str, value_type="avro")
+        product_consumer = cluster.consumer(product_topic_str, value_type="avro")
+        order_consumer = cluster.consumer(order_topic_str, value_type="avro")
+        clicks_int = cluster.l(click_topic_str)[click_topic_str]
+        customers_int = cluster.l(customer_topic_str)[customer_topic_str]
+        products_int = cluster.l(product_topic_str)[product_topic_str]
+        orders_int = cluster.l(order_topic_str)[order_topic_str]
+        n = 0
+        start_time_int = time.time()
+        while True:
+            click_message_dict_list = click_consumer.consume()
+            customer_message_dict_list = customer_consumer.consume()
+            product_message_dict_list = product_consumer.consume()
+            order_message_dict_list = order_consumer.consume()
+            #
+            click_int = click_message_dict_list[-1]["offset"] if len(click_message_dict_list) > 0 else clicks_int - 1
+            customer_int = customer_message_dict_list[-1]["offset"] if len(customer_message_dict_list) > 0 else customers_int - 1
+            product_int = product_message_dict_list[-1]["offset"] if len(product_message_dict_list) > 0 else products_int - 1
+            order_int = order_message_dict_list[-1]["offset"] if len(order_message_dict_list) > 0 else orders_int - 1
+            #
+            click_zset = message_dict_list_to_ZSet(click_message_dict_list)
+            customer_zset = message_dict_list_to_ZSet(customer_message_dict_list)
+            product_zset = message_dict_list_to_ZSet(product_message_dict_list)
+            order_zset = message_dict_list_to_ZSet(order_message_dict_list)
+            #
+            source_click_topologyNode.output_handle_function().get().send(click_zset)
+            source_customer_topologyNode.output_handle_function().get().send(customer_zset)
+            source_product_topologyNode.output_handle_function().get().send(product_zset)
+            source_order_topologyNode.output_handle_function().get().send(order_zset)
+            #
+            root_topologyNode.step()
+            root_topologyNode.gc()
+            #
+            latest_zset = root_topologyNode.latest()
+            print(latest_zset)
+            #
+            len_latest_zset = len(latest_zset.inner.keys())
+            print(f"{len(click_message_dict_list)}/{len(customer_message_dict_list)}/{len_latest_zset} --- {click_int + 1}/{clicks_int}; {customer_int + 1}/{customers_int}; {product_int + 1}/{products_int}; {order_int + 1}/{orders_int}; {order_int + 1}/{orders_int}")
+            n += len_latest_zset
+            #
+            if click_int == clicks_int - 1 and customer_int == customers_int - 1 and product_int == products_int - 1 and order_int == orders_int - 1:
+                break
+        #
+        end_time_int = time.time()
+        #
+        click_consumer.close()
+        customer_consumer.close()
+        product_consumer.close()
+        order_consumer.close()
         #
         print(f"Would have produced {n} outputs in {end_time_int - start_time_int}")
